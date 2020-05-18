cdc454307760bdd2d0cf83144f4c1967fdc3878c
Jakub Ciećwierz <s16878@pjwstk.edu.pl>
2020-05-18 15:49:34 +0200

    Dodaj pliki projektów.

---
+++ Cwiczenia10/Controllers/StudentsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cwiczenia10.Models;
+using Cwiczenia10.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenia10.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        private s16878Context _context;
+        public StudentsController(s16878Context context)
+        {
+            _context = context;
+        }
+     
+        /*
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var list = _context.Student.Where(s => s.FirstName != null).ToList();
+            return Ok(list);
+        }
+        */
+        /*
+        [HttpGet("{id}")]
+
+        public IActionResult Update(string id)
+        {
+            var list = _context.Student.First(s => s.IndexNumber == id);
+            list.FirstName = "Adam";
+            _context.SaveChanges();
+            return Ok("Zaktualizowano");
+        }
+        */
+
+        [HttpGet("{id}")]
+
+        public IActionResult Delete(string id)
+        {
+            _context.Remove(_context.Student.Single(s => s.IndexNumber == id));
+            _context.SaveChanges();
+            return Ok("Usunięto");
+        }
+
+    }
+}
---
+++ Cwiczenia10/Services/EfDbService.cs
@@ -0,0 +1,36 @@
+﻿using Cwiczenia10.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwiczenia10.Services
+{
+    public class EfDbService : IDbServices
+    {
+        public s16878Context _context { get; set; }
+
+        public EfDbService(s16878Context context)
+        {
+            _context = context;
+        }
+        public void PromoteStudents()
+        {
+            var list = _context.Student.Where(s => s.FirstName != null).ToList();
+            
+        }
+
+        public void Update(string id)
+        {
+            var list = _context.Student.First(s => s.IndexNumber == id);
+            list.FirstName = "Adam";
+            _context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            _context.Remove(_context.Student.Single(s => s.IndexNumber == id));
+            _context.SaveChanges();
+        }
+    }
+}
